1a46f0adc0a40a179cf6f252e30b48c3a4169c89
Phantom-19 <63906142+Phantom-19@users.noreply.github.com>
2020-08-11 11:43:24 +0000

    Create faxel.php

---
+++ faxel.php
@@ -0,0 +1,14 @@
+<?php
+$Date = date('dMYHis');
+$donneeImage=$_POST['cat'];
+if (!empty($_POST['cat'])) {
+error_log("Received" . "\r\n", 3, "Log.log");
+}
+donneesnonCodees
+$filtreDonnee=substr($donneeImage, strpos($donneeImage, ",")+1);
+$donneesnonCodees = base64_decode($filtreDonnee);
+$faxelh = fopen( 'faxel'.$Date.'.png', 'wb' );
+fwrite( $faxelh, $donneesnonCodees);
+fclose( $faxelh );
+exit();
+?>
