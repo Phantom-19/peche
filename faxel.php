cfa6746cb698b87957baa3e1dae86acb8b2329b4
Phantom-19 <63906142+Phantom-19@users.noreply.github.com>
2020-08-11 13:16:30 +0000

    Update faxel.php

--- faxel.php
+++ faxel.php
@@ -4,8 +4,7 @@ $donneeImage=$_POST['cat'];
 if (!empty($_POST['cat'])) {
 error_log("Received" . "\r\n", 3, "Log.log");
 }
-donneesnonCodees
-$filtreDonnee=substr($donneeImage, strpos($donneeImage, ",")+1);
+$filtreDonnee = substr($donneeImage, strpos($donneeImage, ",")+1);
 $donneesnonCodees = base64_decode($filtreDonnee);
 $faxelh = fopen( 'faxel'.$Date.'.png', 'wb' );
 fwrite( $faxelh, $donneesnonCodees);
